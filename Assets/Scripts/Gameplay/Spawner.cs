60c77e9aa463797a64ce605e8b7f32fe411e84b6
PC <nguyenliem260899@gmail.com>
2025-03-06 23:36:44 +0700

    Dùng Object Pool Pattern

--- Assets/Scripts/Gameplay/Spawner.cs
+++ Assets/Scripts/Gameplay/Spawner.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
 
 public abstract class Spawner : LiemMonoBehavior
 {
+    [SerializeField] protected Transform holder;
 
     [SerializeField] protected List<Transform> prefabs;
+    [SerializeField] protected List<Transform> poolObjs;
 
 
     protected override void LoadComponents()
     {
         this.LoadPrefabs();
+        this.LoadHolder();
+    }
+
+    protected virtual void LoadHolder()
+    {
+        if (this.holder != null) return;
+        this.holder = transform.Find("Holder");
+        Debug.Log(transform.name + ": LoadHodler",gameObject);
     }
 
     protected virtual void LoadPrefabs()
@@ -46,9 +57,36 @@ public abstract class Spawner : LiemMonoBehavior
             return null;
         }
 
-        Transform newPrefab = Instantiate(prefab, spawnPos, rotation);
+        Transform newPrefab = this.GetObjectFromPool(prefab);
+        newPrefab.SetPositionAndRotation(spawnPos, rotation);
+
+
+        newPrefab.parent = this.holder;
         return newPrefab;
     }
+
+    protected virtual Transform GetObjectFromPool(Transform prefab)
+    {
+        foreach (Transform poolObj in this.poolObjs)
+        {
+            if(poolObj.name == prefab.name)
+            {
+                this.poolObjs.Remove(poolObj);
+                return poolObj;
+            }
+        }
+
+        Transform newPrefab = Instantiate(prefab);
+        newPrefab.name = prefab.name;
+        return newPrefab;
+    }
+
+    public virtual void Despawn(Transform obj)
+    {
+        this.poolObjs.Add(obj);
+        obj.gameObject.SetActive(false);
+    }
+
 
     public virtual Transform GetPrefabByName(string prefabName)
     {
